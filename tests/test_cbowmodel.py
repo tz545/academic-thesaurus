84c7d211af8996b4d6f48407d603a5a2b32cd6ac
Tiantian Zheng <tz545@nyu.edu>
2022-05-31 18:16:12 -0400

    added testing for weight updates

--- tests/test_cbowmodel.py
+++ tests/test_cbowmodel.py
@@ -4,6 +4,7 @@ import pytest
 import os
 from AcademicThesaurus.word2vec_cbow import CBOW_Model, CBOWDataset, Vocabulary
 from torch.utils.data import DataLoader
+from copy import deepcopy
 
 
 def test_model_output_dimensions():
@@ -38,8 +39,31 @@ def test_training_decreases_loss(rootdir):
 	loss2 = criterion(output, batch[1])
 	assert loss2.item() < loss1.item()
 
+def test_training_updates_weights(rootdir):
+	voc = Vocabulary("test")
+	voc.add_wordlist(["one", "two", "three", "four", "five", "six", "seven"])
+	testset = CBOWDataset(os.path.join(rootdir, "test_dataset"), voc, 2)
+	model = CBOW_Model(voc.num_words, 600)
+	params = deepcopy(list(model.parameters()))
+	assert len(params) == 5 # embeddings weight, linear 1 weight, linear 1 bias, linear 2 weight, linear 2 bias
+	criterion = nn.CrossEntropyLoss()
+	optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+	loader = DataLoader(testset, batch_size=3, shuffle=False)
+	optimizer.zero_grad()
+	batch = next(iter(loader))
+	output = model(batch[0])
+	loss = criterion(output, batch[1])
+	loss.backward()
+	optimizer.step()
 
+	trained_params = list(model.parameters())
 
+	assert params[0].data.flatten().tolist() != trained_params[0].data.flatten().tolist()
+	assert params[1].data.flatten().tolist() != trained_params[1].data.flatten().tolist()
+	assert params[2].data.flatten().tolist() != trained_params[2].data.flatten().tolist()
+	assert params[3].data.flatten().tolist() != trained_params[3].data.flatten().tolist()
+	assert params[4].data.flatten().tolist() != trained_params[4].data.flatten().tolist()
+	
 
 
 
